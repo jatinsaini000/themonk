a26460c5c16d677954974de459174b3f9f8e3331
Jatin <sainijatin505@gmail.com>
2026-04-30 14:31:14 +0530

    Configure for Netlify root deployment

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/themonk/',
+  base: '/',
   plugins: [
     tailwindcss(),
   ],
